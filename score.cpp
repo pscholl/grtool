a2c64e1a35277beb5185002f14bf3784906d651b
Philipp M. Scholl <pscholl@ese.uni-freiburg.de>
2015-02-02 08:53:18 +0100

    no more nans in output and report standard deviation

--- score.cpp
+++ score.cpp
@@ -3,18 +3,25 @@
 #include <stdint.h>
 #include <math.h>
 
+// TODO add sorted input
+
 /* some helper functions */
 int    push_back_if_not_there(string &label, vector<string> &labelset);
+string centered(int, string);
 string centered(int, double);
+string meanstd(vector< double >);
 template< class T> vector<T>  diag(Matrix<T> &m);
-template< class T> T          sum(vector<T> &m);
+template< class T> T          sum(vector<T> m);
 template< class T> T          sum(Matrix<T> &m);
+template< class T> vector<T>  abs(vector<T> m);
+template< class T> vector<T>  pow(vector<T> m, double pow);
 template< class T> vector<T>  rowsum(Matrix<T> &m);
 template< class T> vector<T>  colsum(Matrix<T> &m);
 template< class T> Matrix<T>* resize_martix(Matrix<T> *old, T newsize);
 template< class T> vector<T>  operator-(const std::vector<T> &a, const std::vector<T> &b);
 template< class T> vector<T>  operator*(T a, const std::vector<T> &b);
 template< class T> vector<T>  operator-(T a, const std::vector<T> &b);
+template< class T> vector<T>  operator-(const std::vector<T> &b, T a);
 template< class T> vector<T>  operator+(T a, const std::vector<T> &b);
 
 int main(int argc, char *argv[])
@@ -25,7 +32,7 @@ int main(int argc, char *argv[])
   c.add         ("no-confusion",  'c', "report confusion martix");
   c.add         ("no-precision",  'p', "report precision, per class and overall");
   c.add         ("no-recall",     'r', "report recall, per class and overall");
-  c.add<double> ("F-score",       'F', "report F-beta score, beta defaults to 1", false, 1);
+  c.add<double> ("F-score",       'F', "report F-beta score, beta defaults to 1, 0 to disable", false, 1);
   c.footer      ("[filename] ...");
 
   /* parse the classifier-common arguments */
@@ -49,15 +56,28 @@ int main(int argc, char *argv[])
 
   /* open standard input or file argument */
   istream &in = grt_fileinput(c);
+  if (!in) return -1;
 
-  string prediction, label;
+  /* read the whole input to sort it */
+  vector<string> lines; string line;
+  while (getline(in, line))
+    lines.push_back(line);
+  sort(lines.begin(), lines.end());
+
+  if (lines.size() == 0) {
+    cerr << "empty input" << endl;
+    return -1;
+  }
 
   /* prepare the labelset and build confusion matrix */
   vector<string> labelset;
   Matrix<uint64_t> *confusion = new Matrix<uint64_t>();
 
-  while (in >> prediction >> label)
+  for (auto line : lines)
   {
+    stringstream ss(line); string prediction, label;
+    ss >> prediction >> label;
+
     int64_t idxA = push_back_if_not_there(prediction, labelset),
             idxB = push_back_if_not_there(label, labelset);
 
@@ -126,17 +146,18 @@ int main(int argc, char *argv[])
     tab_size += 1;
 
     vector<double> recall, precision, Fbeta;
+    uint64_t TAB_SIZE = 18;
 
     cout << "  " << string(tab_size - 2 + 1, ' ');
-    if (!c.exist("no-recall"))    cout << "  recall  ";
-    if (!c.exist("no-precision")) cout << "  precision  ";
-    if (beta>0)                   cout << "  Fbeta  ";
+    if (!c.exist("no-recall"))    cout << centered(TAB_SIZE,"  recall  ");
+    if (!c.exist("no-precision")) cout << centered(TAB_SIZE,"  precision  ");
+    if (beta>0)                   cout << centered(TAB_SIZE,"  Fbeta  ");
     cout << endl;
 
     cout << string(tab_size, '-') << " " ;
-    if (!c.exist("no-recall"))    cout << " -------- ";
-    if (!c.exist("no-precision")) cout << " ----------- ";
-    if (beta>0)                   cout << " ------- ";
+    if (!c.exist("no-recall"))    cout << string(TAB_SIZE-1, '-') << " ";
+    if (!c.exist("no-precision")) cout << string(TAB_SIZE-1, '-') << " ";
+    if (beta>0)                   cout << string(TAB_SIZE-1, '-');
     cout << endl;
 
     for (size_t i=0; i<labelset.size(); i++) {
@@ -145,32 +166,36 @@ int main(int argc, char *argv[])
       Fbeta.push_back( (1+beta*beta) * TP[i] / ((1+beta*beta) * TP[i] + beta*beta * FN[i] + FN[i]) );
 
       cout << labelset[i] << string(tab_size - labelset[i].size() + 1,' ');
-      if (!c.exist("no-recall"))    cout << centered(sizeof("  recall "), recall.back() );
-      if (!c.exist("no-precision")) cout << centered(sizeof("  precision "), precision.back() );
-      if (beta>0)                   cout << centered(sizeof("  Fbeta "), Fbeta.back() );
+      if (!c.exist("no-recall"))    cout << centered(TAB_SIZE, std::isnan(recall.back())     ? "" : to_string(recall.back()));
+      if (!c.exist("no-precision")) cout << centered(TAB_SIZE, std::isnan(precision.back()) ? "" : to_string(precision.back()));
+      if (beta>0)                   cout << centered(TAB_SIZE, std::isnan(Fbeta.back())     ? "" : to_string(Fbeta.back()));
       cout << endl;
     }
 
     cout << string(tab_size +1, ' ');
-    if (!c.exist("no-recall"))    cout << centered(sizeof("  recall "), sum(recall)/recall.size());
-    if (!c.exist("no-precision")) cout << centered(sizeof("  precision "), sum(precision)/recall.size());
-    if (beta>0)                   cout << centered(sizeof("  Fbeta "), sum(Fbeta)/recall.size());
+    if (!c.exist("no-recall"))      cout << centered(TAB_SIZE, meanstd(recall));
+    if (!c.exist("no-precision"))   cout << centered(TAB_SIZE, meanstd(precision));
+    if (beta>0)                     cout << centered(TAB_SIZE, meanstd(Fbeta));
     cout << endl;
   }
 
   return 0;
 }
 
-string centered(int tab_size, double value) {
+string centered(int tab_size, string val) {
   stringstream ss;
-  string val = to_string(value);
   if (tab_size < 5) tab_size = 5;
-  if (val.size() > 5) val.resize(5);
+  if (val.size() > tab_size-4) val.resize(tab_size-4);
+  //if (val.size() > 5) val.resize(5);
   int pre = (tab_size - val.size()) / 2,
      post =  tab_size - val.size() - pre;
 
   ss << string(pre,' ') << val << string(post, ' ');
   return ss.str();
+}
+
+string centered(int tab_size, double value) {
+  return centered(tab_size, to_string(value));
 }
 
 template< class T>
@@ -182,7 +207,7 @@ vector<T> diag(Matrix<T> &m) {
 }
 
 template< class T>
-T sum(vector<T> &m) {
+T sum(vector<T> m) {
   T result = m[0];
   for (int i=1; i<m.size(); i++)
     result += m[i];
@@ -231,6 +256,26 @@ vector<T>  colsum(Matrix<T> &m) {
   return result;
 }
 
+template< class T>
+vector<T> abs(vector<T> m) {
+  vector<T> result;
+  for (int i=0; i<m.size(); i++) {
+    T val = m[i];
+    result.push_back( std::abs(val) );
+  }
+  return result;
+}
+
+template< class T>
+vector<T>  pow(vector<T> m, double pow) {
+  vector<T> result;
+  for (int i=0; i<m.size(); i++) {
+    T val = std::pow(m[i], pow);
+    result.push_back( val );
+  }
+  return result;
+}
+
 int push_back_if_not_there(string &label, vector<string> &labelset)
 {
  if( std::find(labelset.begin(), labelset.end(), label) == labelset.end() )
@@ -256,6 +301,14 @@ template< class T> vector<T>  operator-(T a, const std::vector<T> &b)
   return res;
 }
 
+template< class T> vector<T>  operator-(const std::vector<T> &b, T a)
+{
+  vector<T> res(b.size());
+  for (size_t i=0; i<b.size(); ++i)
+    res[i]=b[i]-a;
+  return res;
+}
+
 template< class T> vector<T>  operator*(T a, const std::vector<T> &b)
 {
   vector<T> res(b.size());
@@ -272,3 +325,22 @@ template< class T> vector<T>  operator+(T a, const std::vector<T> &b)
   return res;
 }
 
+string meanstd(vector< double > list) {
+  vector< double > cleaned;
+
+  for ( auto val : list )
+    if (!std::isnan(val))
+      cleaned.push_back(val);
+
+  if (cleaned.size() == 0)
+    return "";
+
+  double mean = sum(cleaned)/cleaned.size();
+
+  if (cleaned.size() == 1)
+    return to_string(mean);
+
+  stringstream ss;
+  ss << mean << "/" << sqrt( sum(pow(abs(cleaned-mean),2))/cleaned.size() );
+  return ss.str();
+}
